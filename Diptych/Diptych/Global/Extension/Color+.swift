87266e69753bfd57d2b8d910c56fb9e9d07d3f10
Mini <minnnidev@gmail.com>
2023-07-13 03:09:17 +0900

    [Setting] #2 - 컬러 정리

--- Diptych/Diptych/Global/Extension/Color+.swift
+++ Diptych/Diptych/Global/Extension/Color+.swift
@@ -5,6 +5,7 @@
 //  Created by 김민 on 2023/07/13.
 //
 
+import SwiftUI
 import UIKit
 
 extension UIColor {
@@ -12,4 +13,20 @@ extension UIColor {
     // MARK: - Custom Color
 
     static var darkGray: UIColor? { return UIColor(named: "DarkGray") }
+}
+
+extension Color {
+
+    // MARK: - Custom Color
+
+    static let darkGray = Color("DarkGray")
+    static let lightGray = Color("LightGray")
+    static let offBlack = Color("OffBlack")
+    static let offBlack50 = Color("OffBlack50")
+    static let offWhite = Color("OffWhite")
+    static let red = Color("Red")
+    static let salmon = Color("Salmon")
+    static let systemBlack = Color("SystemBlack")
+    static let systemBlack85 = Color("SystemBlack85")
+    static let systemWhite = Color("SystemWhite")
 }
