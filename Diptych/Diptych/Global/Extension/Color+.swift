2fa9d07033c47cfb09e1d54ccf70f2a2d104846f
Mini <minnnidev@gmail.com>
2023-07-13 02:21:26 +0900

    [Design] #2 - 하단 탭바 구현 및 해당 파일 생성

---
+++ Diptych/Diptych/Global/Extension/Color+.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+.swift
+//  Diptych
+//
+//  Created by 김민 on 2023/07/13.
+//
+
+import UIKit
+
+extension UIColor {
+
+    // MARK: - Custom Color
+
+    static var darkGray: UIColor? { return UIColor(named: "DarkGray") }
+}
---
+++ Diptych/Diptych/Presentation/Profile/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Diptych
+//
+//  Created by 김민 on 2023/07/13.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("ProfileView")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
