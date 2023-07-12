32b71028d9a9fb6fd856fd7373e09259c4319d21
Mini <minnnidev@gmail.com>
2023-07-13 02:21:26 +0900

    [Design] #1 - 하단 탭바 구현 및 해당 파일 생성

---
+++ Diptych/Diptych/Presentation/Archive/View/ArchiveView.swift
@@ -0,0 +1,20 @@
+//
+//  ArchiveView.swift
+//  Diptych
+//
+//  Created by 김민 on 2023/07/13.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        Text("Archive")
+    }
+}
+
+struct ArchiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArchiveView()
+    }
+}
