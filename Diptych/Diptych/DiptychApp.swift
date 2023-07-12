913b23d1a740541788f922dfa116c3a2866a27e5
Mini <minnnidev@gmail.com>
2023-07-12 21:22:01 +0900

    [Setting] 프로젝트 세팅

---
+++ Diptych/Diptych/DiptychApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiptychApp.swift
+//  Diptych
+//
+//  Created by 김민 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct DiptychApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Diptych/DiptychUITests/DiptychUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DiptychUITestsLaunchTests.swift
+//  DiptychUITests
+//
+//  Created by 김민 on 2023/07/12.
+//
+
+import XCTest
+
+final class DiptychUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
