39545fa4a13264197e19bdc2cbecb81e064b25ab
Mini <minnnidev@gmail.com>
2023-07-13 11:14:00 +0900

    [Setting] #1 - 폰트 추가 및 정리

---
+++ Diptych/Diptych/Util/NameSpace/PretendardType.swift
@@ -0,0 +1,15 @@
+//
+//  PretendardType.swift
+//  Diptych
+//
+//  Created by 김민 on 2023/07/13.
+// 
+
+import Foundation
+
+public enum PretendardType: String {
+    
+    case bold = "Pretendard-Bold"
+    case light = "Pretendard-Light"
+    case medium = "Pretendard-Medium"
+}
