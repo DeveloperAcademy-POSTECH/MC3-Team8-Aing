fe3357d259e4e7cbb666ca29c9bf21f3032837f9
Mini <minnnidev@gmail.com>
2023-07-13 11:23:57 +0900

    [Add] #1 - 폰트 추가

--- Diptych/Diptych/Util/NameSpace/PretendardType.swift
+++ Diptych/Diptych/Util/NameSpace/PretendardType.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 public enum PretendardType: String {
+    case black = "Pretendard-Black"
     case bold = "Pretendard-Bold"
+    case extraBold = "Pretendard-ExtraBold"
+    case extraLight = "Pretendard-ExtraLight"
     case light = "Pretendard-Light"
     case medium = "Pretendard-Medium"
+    case regular = "Pretendard-Regular"
+    case semiBold = "Pretendard-SemiBold"
+    case thin = "Pretendard-Thin"
 }
