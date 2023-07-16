4268ebf29389cae3d57a40cffeb9a9cdfa0d8411
BGSMM <yoonbumtae@gmail.com>
2023-07-16 21:44:12 +0900

    [Design]: 미리보기 화면비 1:1로 조정

--- Diptych/Diptych/Presentation/TodayDiptych/ViewController/CameraViewController.swift
+++ Diptych/Diptych/Presentation/TodayDiptych/ViewController/CameraViewController.swift
@@ -11,6 +11,7 @@ import Photos
 
 class CameraViewController: UIViewController {
     
+    // MARK: - 스토리보드로부터 @IBOutlet으로 연결된 컴포넌트 변수
     @IBOutlet weak var lblTopic: UILabel!
     @IBOutlet weak var imgViewGuideOverlay: UIImageView!
     
@@ -20,101 +21,32 @@ class CameraViewController: UIViewController {
     var backCameraInput: AVCaptureDeviceInput!
     var frontCameraInput: AVCaptureDeviceInput!
     
-    var photoSettings: AVCapturePhotoSettings!
-    var photoOutput: AVCapturePhotoOutput = {
-        let output = AVCapturePhotoOutput()
-        // 고해상도의 이미지 캡쳐 가능 설정
-        if #available(iOS 16.0, *) {
-            // output.maxPhotoDimensions = .init()
-            output.isHighResolutionCaptureEnabled = true
+    var photoSettings: AVCapturePhotoSettings {
+        // NSInvalidArgumentException', reason: '*** -[AVCapturePhotoOutput capturePhotoWithSettings:delegate:]
+        // Settings may not be re-used'
+        
+        var settings = AVCapturePhotoSettings()
+        // photoOutput 의 codec의 hevc 가능시 photoSettings의 codec을 hevc로 설정하는 코드입니다.
+        // hevc 불가능한 경우에는 jpeg codec을 사용하도록 합니다.
+        if photoOutput.availablePhotoCodecTypes.contains(.hevc) {
+            settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
         } else {
-            output.isHighResolutionCaptureEnabled = true
+            settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
         }
         
-        return output
-    }()
+        return settings
+    }
+    var photoOutput: AVCapturePhotoOutput = AVCapturePhotoOutput()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        
-        
         checkCameraPermissions()
-        // 1
-        DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
-            // 세션 초기화
-            captureSession = AVCaptureSession()
-            // 구성(configuration) 시작
-            captureSession.beginConfiguration()
-            
-            // session specific configuration
-            // 세션 프리셋을 설정하기 전에 지원 여부를 확인해야 합니다.
-            if captureSession.canSetSessionPreset(.photo) {
-                captureSession.sessionPreset = .photo
-            }
-            
-            // 사용 가능한 경우 세션이 자동으로 광역 색상을 사용해야 하는지 여부를 지정합니다.
-            captureSession.automaticallyConfiguresCaptureDeviceForWideColor = true
-            
-            // Setup inputs (분리)
-            
-            // setupInputs()
-            // guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-
-            // 후면 카메라
-            guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
-                return
-            }
-
-            // 전면 카메라
-            guard let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
-                return
-            }
-            
-            // 생성된 captureSession에 device input을 생성 및 연결하고,
-            // device output을 연결하는 코드입니다.
-            
-            
-            
-            
-            
-            do {
-                backCameraInput = try AVCaptureDeviceInput(device: backCamera)
-                frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
-                    
-                // captureSession에 cameraInput을 받도록 설정
-                captureSession.addInput(backCameraInput)
-                captureSession.addOutput(photoOutput)
-                
-                // photoOutput 의 codec의 hevc 가능시 photoSettings의 codec을 hevc 로 설정하는 코드입니다.
-                // hevc 불가능한 경우에는 jpeg codec을 사용하도록 합니다.
-                if photoOutput.availablePhotoCodecTypes.contains(.hevc) {
-                    photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
-                } else {
-                    photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
-                }
-            } catch  {
-                
-            }
-            
-            // Preview
-            DispatchQueue.main.async { [unowned self] in
-                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                view.layer.insertSublayer(previewLayer, above: imgViewGuideOverlay.layer)
-                previewLayer.frame = self.view.frame
-            }
-            
-            // commit configuration: 단일 atomic 업데이트에서 실행 중인 캡처 세션의 구성에 대한 하나 이상의 변경 사항을 커밋합니다.
-            self.captureSession.commitConfiguration()
-            // 캡처 세션 실행
-            captureSession.startRunning()
-        }
+        setupPhotoCamera()
     }
     
     @IBAction func btnActShutter(_ sender: UIButton) {
@@ -142,8 +74,78 @@ class CameraViewController: UIViewController {
         captureSession.commitConfiguration()
     }
     
+    // MARK: - Camera Functions
+    
+    func setupPhotoCamera() {
+        // 메인 스레드에서 실행 방지 - startRunning()이 차단 호출(block call)이기 때문
+        DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
+            // 세션 초기화
+            captureSession = AVCaptureSession()
+            // 구성(configuration) 시작
+            captureSession.beginConfiguration()
+            
+            // session specific configuration
+            // 세션 프리셋을 설정하기 전에 지원 여부를 확인해야 합니다.
+            if captureSession.canSetSessionPreset(.photo) {
+                captureSession.sessionPreset = .photo
+            }
+            
+            // 사용 가능한 경우 세션이 자동으로 광역 색상을 사용해야 하는지 여부를 지정합니다.
+            captureSession.automaticallyConfiguresCaptureDeviceForWideColor = true
+
+            // 후면 카메라 디바이스
+            guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+                return
+            }
+
+            // 전면 카메라 디바이스
+            guard let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+                return
+            }
+            
+            // 생성된 captureSession에 device input을 생성 및 연결하고,
+            // device output을 연결하는 코드입니다.
+            do {
+                backCameraInput = try AVCaptureDeviceInput(device: backCamera)
+                frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
+                    
+                // captureSession에 cameraInput을 받도록 설정
+                captureSession.addInput(backCameraInput)
+            } catch  {
+                debugPrint("Camera Input Error:", error.localizedDescription)
+            }
+            
+            // 고해상도의 이미지 캡처 가능 설정
+            // 16.0에서 deprecated됨
+            if #unavailable(iOS 16.0) {
+                photoOutput.isHighResolutionCaptureEnabled = true
+            }
+            
+            // CaptureSession에 photoOutput을 추가
+            captureSession.addOutput(photoOutput)
+            
+            // Preview 화면 추가
+            DispatchQueue.main.async { [unowned self] in
+                setCameraPreview()
+            }
+            
+            // commit configuration: 단일 atomic 업데이트에서 실행 중인 캡처 세션의 구성에 대한 하나 이상의 변경 사항을 커밋합니다.
+            captureSession.commitConfiguration()
+            // 캡처 세션 실행
+            captureSession.startRunning()
+        }
+    }
+    
+    func setCameraPreview() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.insertSublayer(previewLayer, above: imgViewGuideOverlay.layer)
+        previewLayer.frame = imgViewGuideOverlay.frame
+        previewLayer.videoGravity = .resizeAspectFill   // 프레임 크기에 맞춰 리사이즈(비율 깨지지 않음)
+    }
+    
     
     // MARK: - Permissions
+    
     func checkCameraPermissions() {
         let cameraAuthStatus =  AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
         switch cameraAuthStatus {
@@ -170,7 +172,7 @@ class CameraViewController: UIViewController {
 }
 
 extension CameraViewController: AVCapturePhotoCaptureDelegate {
-    /// capturePhoto 이후에 capture process 가 완료된 이미지를 저장하는 메쏘드
+    /// capturePhoto 이후에 capture process 가 완료된 이미지를 저장하는 메서드
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         guard error == nil else {
             print("Error capturing photo:", error!.localizedDescription)
