8e8372f6943464d4327fd298836c628d76470080
Taeyun Koo <hardestpart1503@gmail.com>
2023-07-18 18:00:36 +0900

    [Design] #15 - 주석 처리 (일단)

--- Diptych/Diptych/Presentation/Archive/ViewModel/AlbumViewModel.swift
+++ Diptych/Diptych/Presentation/Archive/ViewModel/AlbumViewModel.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Koo on 2023/07/18.
 //
-
-import SwiftUI
-
-class AlbumViewModel: ObservableObject {
+//
+//import SwiftUI
+//
+//class AlbumViewModel: ObservableObject {
     
     // property
     // Json 파일을 가져다 SwiftUI에서 사용할 수 있도록 []안에 저장
-    @Published var albumData: [AlbumData] = Bundle.main.decode("AlbumData.json")
+//    @Published var albumData: [AlbumData] = Bundle.main.decode("AlbumData.json")
