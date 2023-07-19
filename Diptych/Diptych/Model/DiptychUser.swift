7ad67dd7d5bf033a0aea8a7f526a4458a380ee0c
BGSMM <yoonbumtae@gmail.com>
2023-07-19 17:45:19 +0900

    fix error

---
+++ Diptych/Diptych/Model/DiptychUser.swift
@@ -0,0 +1,22 @@
+//
+//  DiptychUser.swift
+//  Diptych
+//
+//  Created by HAN GIBAEK on 2023/07/18.
+//
+
+import Foundation
+
+struct DiptychUser: Identifiable, Codable {
+    let id: String
+    let email: String
+    let name: String
+    
+    var flow: String
+    var sharedAlbumId: String?
+    var nickname: String?
+    var phoneNumber: String?
+    var couplingCode: String?
+    var loverId: String?
+    var startDay: Date?
+}
---
+++ Diptych/Diptych/Presentation/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,67 @@
+//
+//  OnBoardingView.swift
+//  Diptych
+//
+//  Created by HAN GIBAEK on 2023/07/13.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+//    @EnvironmentObject var authViewModel: AuthenticationViewModel
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.offWhite
+                    .ignoresSafeArea()
+                VStack() {
+                    Spacer()
+                        .frame(height: 124)
+                    Text("딥틱에서 매일 서로의 일상을 하나의 사진으로 완성해요")
+                        .font(.pretendard(.light, size: 28))
+                        .padding(.leading, 41)
+                        .padding(.trailing, 41)
+                    Spacer()
+                        .frame(height: 28)
+                    Image("diptych_sample1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 393, height: 393)
+                    Spacer()
+                    VStack(spacing: 10) { // (S) LogIn, SignUp
+                        NavigationLink(destination: LogInView()) {
+                            Text("로그인")
+                                .frame(width: UIScreen.main.bounds.width-30, height:  60)
+                                .background(Color.offBlack)
+                                .foregroundColor(.offWhite)
+                                .border(Color.offBlack)
+                        }
+                        NavigationLink(destination: SignUpView()) {
+                            Text("회원가입")
+                                .frame(width: UIScreen.main.bounds.width-30, height:  60)
+                                .background(Color.offWhite)
+                                .foregroundColor(.offBlack)
+                                .border(Color.offBlack)
+                        }
+                        
+                    }// (E) LogIn, SignUp
+                    Spacer()
+                        .frame(height: 55)
+                }
+                .ignoresSafeArea()
+            }
+//            .onAppear {
+//                print("user: \(authViewModel.user)")
+//                print("user: \(authViewModel.user?.isEmailVerified)")
+//                print("state: \(authViewModel.authenticationState)")
+//            }
+        }
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+            .environmentObject(AuthenticationViewModel())
+    }
+}
