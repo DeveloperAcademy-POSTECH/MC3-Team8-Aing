f758879bfacbbd5df91f6a79a107e1b0dc477ca0
Mini <minnnidev@gmail.com>
2023-08-14 04:39:31 +0900

    [Setting] #108 - 폴더 구조 수정

--- Diptych/Diptych/Global/Extension/Date+.swift
+++ Diptych/Diptych/Global/Extension/Date+.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension Date {
+    
     func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
         return calendar.dateComponents(Set(components), from: self)
     }
