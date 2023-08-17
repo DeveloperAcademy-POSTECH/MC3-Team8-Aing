3e5a7766f509152ecee57016ab2fc24457a0f568
윤범태 <yoonbumtae@gmail.com>
2023-08-18 01:50:25 +0900

    [Fix] 스크롤 타이머 시간 변경

--- Diptych/Diptych/Presentation/Archive/View/Calendar/CalendarScrollView.swift
+++ Diptych/Diptych/Presentation/Archive/View/Calendar/CalendarScrollView.swift
@@ -35,10 +35,10 @@ struct CalendarScrollView: View {
                      Timer가 들어간 이유:
                      LazyVGrid로 인해 스크롤이 되어야만 사이즈가 정확하게 계산되기 때문에
                      처음에 한 번만 scrollTo를 하면 제 위치에 안들어감.
-                     매우 짧은 시간(약 0.01초)에 5~6번을 새로고침해서 스크롤한 뒤 모든 부분이 로딩되면 제 위치로 스크롤됨.
+                     짧은 시간에 5~6번을 새로고침해서 스크롤한 뒤 모든 부분이 로딩되면 제 위치로 스크롤됨.
                      전혀 바람직한 코드가 아니며 (1)LazyVGrid와 (2)역방향이 최신인 컨텐츠를 고려한 다른 스크롤 방법을 전면적으로 생각해야됨.
                      */
-                    Timer.scheduledTimer(withTimeInterval: 0.002, repeats: true) { timer in
+                    Timer.scheduledTimer(withTimeInterval: 0.025, repeats: true) { timer in
                         if timerCount <= 5 {
                             scrollViewProxy.scrollTo(scrollToID, anchor: .bottom)
                             timerCount += 1
