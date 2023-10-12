088fffb0404e5db1c4bf7a922cde7c128cd13ce2
윤범태 <yoonbumtae@gmail.com>
2023-10-12 22:35:10 +0900

    [Feat] 코멘트 스크롤 뷰 추가 및 뷰모델 생성

---
+++ Diptych/Diptych/Presentation/Comment/View/CommentListScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentListScrollView.swift
+//  Diptych
+//
+//  Created by 윤범태 on 2023/10/12.
+//
+
+import SwiftUI
+
+struct CommentListScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentListScrollView()
+}
---
+++ Diptych/Diptych/Presentation/Comment/ViewModel/CommentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentViewModel.swift
+//  Diptych
+//
+//  Created by 윤범태 on 2023/10/12.
+//
+
+import Foundation
